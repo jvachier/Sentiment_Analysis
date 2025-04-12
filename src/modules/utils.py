0fa134efaffb106889e554d70bf6b15f53f57a47
Jeremy Vachier <89128100+jvachier@users.noreply.github.com>
2025-04-12 17:12:52 +0200

    Updating tests

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -7,8 +7,7 @@ class DatasetPaths(str, Enum):
     Enum for dataset-related paths.
     """
 
-    RAW_DATA = Path("./data/tripadvisor_hotel_reviews.csv")
-    PROCESSED_DATA = Path("./data/processed_data.csv")
+    RAW_DATA = Path("src/data/tripadvisor_hotel_reviews.csv")
 
 
 class ModelPaths(str, Enum):
@@ -16,10 +15,10 @@ class ModelPaths(str, Enum):
     Enum for model-related paths.
     """
 
-    MODEL_BUILDER_CONFIG = Path("./models/model_builder_config.json")
-    MODEL_TRAINER_CONFIG = Path("./models/model_trainer_config.json")
-    TRAINED_MODEL = Path("./models/sentiment_keras_binary.keras")
-    INFERENCE_MODEL = Path("./models/inference_model.keras")
+    MODEL_BUILDER_CONFIG = Path("src/configurations/model_builder_config.json")
+    MODEL_TRAINER_CONFIG = Path("src/configurations/model_trainer_config.json")
+    TRAINED_MODEL = Path("src/models/sentiment_keras_binary.keras")
+    INFERENCE_MODEL = Path("src/models/inference_model.keras")
 
 
 class OptunaPaths(str, Enum):
@@ -27,8 +26,8 @@ class OptunaPaths(str, Enum):
     Enum for Optuna-related paths.
     """
 
-    OPTUNA_CONFIG = Path("./optuna_config.json.json")
-    OPTUNA_MODEL = Path("./models/optuna_model_binary.json")
+    OPTUNA_CONFIG = Path("src/configurations/optuna_config.json")
+    OPTUNA_MODEL = Path("src/models/optuna_model_binary.json")
 
 
 class TextVectorizerConfig(int, Enum):
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,47 +1,54 @@
+import json
 from modules.model import ModelTrainer, OptunaOptimizer, ModelBuilder
+from modules.utils import ModelPaths, OptunaPaths
 
 
 def test_model_trainer_initialization():
     """
-    Test if ModelTrainer initializes with correct values.
+    Test if ModelTrainer initializes with correct values from the configuration file.
     """
-    trainer = ModelTrainer(learning_rate=0.001, epochs=10)
-    assert trainer.learning_rate == 0.001
-    assert trainer.epochs == 10
+    with open(ModelPaths.MODEL_TRAINER_CONFIG.value, "r") as config_file:
+        config = json.load(config_file)
+
+    trainer = ModelTrainer(config_path=ModelPaths.MODEL_TRAINER_CONFIG.value)
+    assert trainer.learning_rate == config["learning_rate"]
+    assert trainer.epochs == config["epochs"]
 
 
 def test_optuna_optimizer_initialization():
     """
-    Test if OptunaOptimizer initializes with correct values.
+    Test if OptunaOptimizer initializes with correct values from the configuration file.
     """
-    optimizer = OptunaOptimizer(max_token=15000, embedding_dim=100, dropout_rate=0.3)
-    assert optimizer.max_token == 15000
-    assert optimizer.embedding_dim == 100
-    assert optimizer.dropout_rate == 0.3
+    with open(OptunaPaths.OPTUNA_CONFIG.value, "r") as config_file:
+        config = json.load(config_file)
+
+    optimizer = OptunaOptimizer(config_path=OptunaPaths.OPTUNA_CONFIG.value)
+    assert optimizer.max_token == config["max_token"]
+    assert optimizer.embedding_dim == config["embedding_dim"]
+    assert optimizer.dropout_rate == config["dropout_rate"]
 
 
 def test_model_builder_initialization():
     """
-    Test if ModelBuilder initializes with correct values.
+    Test if ModelBuilder initializes with correct values from the configuration file.
     """
-    builder = ModelBuilder(
-        embedding_dim=64, lstm_units=256, dropout_rate=0.4, max_token=25000
-    )
-    assert builder.embedding_dim == 64
-    assert builder.lstm_units == 256
-    assert builder.dropout_rate == 0.4
-    assert builder.max_token == 25000
+    with open(ModelPaths.MODEL_BUILDER_CONFIG.value, "r") as config_file:
+        config = json.load(config_file)
+
+    builder = ModelBuilder(config_path=ModelPaths.MODEL_BUILDER_CONFIG.value)
+    assert builder.embedding_dim == config["embedding_dim"]
+    assert builder.lstm_units == config["lstm_units"]
+    assert builder.dropout_rate == config["dropout_rate"]
+    assert builder.max_token == config["max_token"]
 
 
 def test_model_builder_get_model_api():
     """
     Test if ModelBuilder's get_model_api method returns a valid Keras model.
     """
-    builder = ModelBuilder(
-        embedding_dim=64, lstm_units=128, dropout_rate=0.5, max_token=20000
-    )
+    builder = ModelBuilder(config_path=ModelPaths.MODEL_BUILDER_CONFIG.value)
     model = builder.get_model_api()
     assert model is not None
-    assert len(model.layers) == 10  # Check the number of layers in the model
+    assert len(model.layers) > 0  # Check that the model has layers
     assert model.input_shape == (None, None)  # Check input shape
     assert model.output_shape == (None, 1)  # Check output shape
