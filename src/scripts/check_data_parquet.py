172faa1c631d0028fbb1bc2d6c5e79e7fa385af7
Jeremy Vachier <89128100+jvachier@users.noreply.github.com>
2025-04-17 19:33:21 +0200

    Improving the dataframe with polars.

--- src/scripts/check_data_parquet.py
+++ src/scripts/check_data_parquet.py
@@ -8,5 +8,17 @@ print(
     df.describe()
 )  # Polars does not have `info()`, but `describe()` provides summary statistics
 
+
+# Define the delimiters for splitting
+delimiters = r"|"
+
+# Split the 'en' column into rows based on delimiters
+if "en" in df.columns:
+    en_split = df.select(pl.col("en").str.split(delimiters)).explode("en")
+
+# Split the 'fr' column into rows based on delimiters
+if "fr" in df.columns:
+    fr_split = df.select(pl.col("fr").str.split(delimiters)).explode("fr")
+split_df = pl.concat([en_split, fr_split], how="horizontal")
 # Display the first few rows
-print(df.head())
+print(split_df.head(20))
